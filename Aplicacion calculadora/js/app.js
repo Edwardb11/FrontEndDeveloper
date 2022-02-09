3e194afe734e08252fe40d1bbf749bca343a6432
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-09 15:01:48 -0400

    agregando las demas operaciones y un boton para restablecer

--- Aplicacion calculadora/js/app.js
+++ Aplicacion calculadora/js/app.js
@@ -4,9 +4,34 @@ function sumar(){
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
-    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    let resultado='';
+    if(document.getElementById('Suma').checked) {
+        resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    }
+    else if(document.getElementById('Resta').checked){
+        resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    }
+    else if(document.getElementById('Multiplicacion').checked){
+        resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    }
+    else if(document.getElementById('Division').checked){
+        if (parseInt(operandoB.value) ===0) {
+            document.getElementById('resultado').innerHTML = `Resultado: No se puede dividir entre 0`;
+            return
+        }
+        resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    }
     if(isNaN(resultado))
         resultado = 'La operación no incluye números';
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
     console.log(`Resultado: ${resultado}`);
+}
+function reset(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    operandoA.value=''
+    operandoB.value=''
+    document.getElementById('resultado').innerHTML = ' ';
+
 }
