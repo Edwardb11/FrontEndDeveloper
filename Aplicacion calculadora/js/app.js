81d062d8b89dbc1d204d46cf3d32dd3d0c8aa3fa
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-09 15:21:31 -0400

    agregando campo select y agregando exponenciacion

--- Aplicacion calculadora/js/app.js
+++ Aplicacion calculadora/js/app.js
@@ -1,37 +1,47 @@
-console.log('Aplicación Calculadora');
+console.log("Aplicación Calculadora");
 
-function sumar(){
-    const forma = document.getElementById('forma');
-    let operandoA = forma['operandoA'];
-    let operandoB = forma['operandoB'];
-    let resultado='';
-    if(document.getElementById('Suma').checked) {
-        resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-    }
-    else if(document.getElementById('Resta').checked){
-        resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
-    }
-    else if(document.getElementById('Multiplicacion').checked){
-        resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
-    }
-    else if(document.getElementById('Division').checked){
-        if (parseInt(operandoB.value) ===0) {
-            document.getElementById('resultado').innerHTML = `Resultado: No se puede dividir entre 0`;
-            return
-        }
-        resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
-    }
-    if(isNaN(resultado))
-        resultado = 'La operación no incluye números';
-    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-    console.log(`Resultado: ${resultado}`);
+function sumar() {
+  const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  let resultado = "";
+  let operación = document.getElementById("operacion").value;
+  console.log(operación);
+  switch (operación) {
+    case "Suma":
+      resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+      break;
+    case "Resta":
+      resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+      break;
+    case "Multiplicacion":
+      resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+      break;
+    case "Division":
+      if (parseInt(operandoB.value) === 0) {
+        document.getElementById(
+          "resultado"
+        ).innerHTML = `Resultado: No se puede dividir entre 0`;
+        return;
+      }
+      resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+      break;
+
+    case "exponenciacion":
+      resultado = (parseInt(operandoA.value) ** parseInt(operandoB.value));
+      break;
+    default:
+      resultado = "Ha ocurrido un error, intente de nuevo.";
+      break;
+  }
+  if (isNaN(resultado)) resultado = "La operación no incluye números";
+  document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
 }
-function reset(){
-    const forma = document.getElementById('forma');
-    let operandoA = forma['operandoA'];
-    let operandoB = forma['operandoB'];
-    operandoA.value=''
-    operandoB.value=''
-    document.getElementById('resultado').innerHTML = ' ';
-
+function reset() {
+  const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  operandoA.value = "";
+  operandoB.value = "";
+  document.getElementById("resultado").innerHTML = " ";
 }
